03f3eb56fb969f4ca07ba1c353bdbc7eacc37b70
pengdaqian.0 <pengdaqian.0@bytedance.com>
2022-02-21 15:36:02 +0800

    init

--- errorx.go
+++ errorx.go
@@ -1,20 +1,25 @@
 package errorx
 
-type Errorx struct {
+type Errorx interface {
+	Error() string
+	Code() int32
+}
+
+type Err struct {
 	code    int32
 	message string
 }
 
-func (x Errorx) Error() string {
+func (x Err) Error() string {
 	return x.message
 }
 
-func (x Errorx) Code() int32 {
+func (x Err) Code() int32 {
 	return x.code
 }
 
-func NewErrorx(code int32, message string) *Errorx {
-	return &Errorx{
+func NewErrorx(code int32, message string) Errorx {
+	return Err{
 		code:    code,
 		message: message,
 	}
